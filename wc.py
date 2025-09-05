8bd15fe6bf61a0cf49209907b31b5db1214f6a22
Sriharshavenkat129 <sriharshavenkat123@gmail.com>
2025-09-05 09:46:28 +0530

    Update wc.py

--- wc.py
+++ wc.py
@@ -94,8 +94,8 @@ elif st.session_state.input_mode == "url":
 if "vmode" not in st.session_state:
     st.session_state.vmode=None
 
-col1=st.columns(1)[0]
-col2=st.columns(1)[0]
+col1,col2=st.columns(1)[0],st.columns(1)[1]
+#col2=st.columns(1)[0]
 with col1:
     if st.button("WordCloud"):
         visualize(text)
